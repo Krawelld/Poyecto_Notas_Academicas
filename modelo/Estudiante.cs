e6421e4be48ea3662f8b8956cc047ebc59dd7f2c
Krawelld <jean.valencia463@pascualbravo.edu.co>
2026-05-26 14:10:49 -0500

    Creacion de las clases

---
+++ modelo/Estudiante.cs
@@ -0,0 +1,98 @@
+namespace SistemaNotas.Modelo
+{
+    public class Estudiante : Persona
+    {
+        // ATRIBUTOS PRIVADOS
+        private double promedio;
+        private string grado;
+        private Materia materia;
+        private List<Nota> notas;
+
+        // PROPIEDADES PÚBLICAS
+        public double Promedio
+        {
+            get { return promedio; }
+            set 
+            { 
+                if(value >= 0)
+                {
+                    promedio = value;
+                }
+                else
+                {
+                    Console.WriteLine("El promedio debe ser positivo");
+                }
+            }
+        }
+        public string Grado
+        {
+            get { return grado; }
+            set { grado = value; }
+        }
+
+        public Materia Materia
+        {
+            get {return materia; }
+            set {materia = value; }
+        }
+
+        //Lista para guardar notas
+        public List<Nota> Notas
+        {
+            get { return notas; }
+        }
+
+        // CONSTRUCTOR
+        public Estudiante(
+            int id,
+            int edad,
+            string nombre,
+            string documento,
+            string correo,
+            string grado,
+            double promedio
+        ) : base(id, edad, nombre, documento, correo)
+        {
+            this.promedio = promedio;
+            this.grado = grado;
+            notas = new List<Nota>();
+        }
+
+         // VER NOTAS
+        public string VerNotas()
+        {
+            string resultado = "NOTAS DEL ESTUDIANTE\n\n";
+
+            foreach(Nota nota in notas)
+            {
+                resultado += nota.MostrarNota() + "\n\n";
+            }
+
+            return resultado;
+        }
+
+        // VER MATERIAS
+        public string VerMaterias()
+        {
+            string resultado = "MATERIAS DEL ESTUDIANTE\n\n";
+
+            foreach(Nota nota in notas)
+            {
+                resultado +=
+                    nota.Materia.Nombre + "\n";
+            }
+
+            return resultado;
+        }
+
+        // MÉTODO OVERRIDE
+        public override string MostrarInformacion()
+        {
+            return base.MostrarInformacion() +
+                   $"\nPromedio: {promedio}" +
+                   $"\nGrado: {grado}";
+        }
+
+        
+    }
+}
